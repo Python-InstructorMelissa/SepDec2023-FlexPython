f2a3de119616a372f7c696a0b264c79fe6fad8bd
Melissa Longenberger <mlongenberger@codingdojo.com>
2023-09-21 21:26:25 -0400

    saving

---
+++ lectures/week04/day2/termGame.sql
@@ -0,0 +1,8 @@
+SELECT * FROM animal;
+insert into animal (name, species, apendage) values ("Pork","Mammal",5);
+insert into animal (name, species, apendage) values ("Chris P. Bacon", "Pig", 5);
+insert into animal (name, species, apendage) values ("Lily", "Bee", 6), ("Godzilla","Komodo Dragon",6);
+
+
+SELECT * FROM animalFriends;
+insert into animalFriends (animal_id, animal_id1) values (5,6);
